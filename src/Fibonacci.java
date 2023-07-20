a954ff8471015247e1f435fc3fe00f55c39e87ea
RoshanEraniyan <e.roshan141@gmail.com>
2023-07-20 05:13:25 -1200

    Initial Commit Done

---
+++ src/Fibonacci.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+
+class FibonacciNumber
+{
+    public static int fib(int n)
+    {
+        if(n==1)
+        {
+            return 0;
+        }
+        if(n==2)
+        {
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String args[])
+    {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println(fib(n));
+    }
+}
